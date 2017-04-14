6e4469f74ea9de18b949aa9bd62febf4b5518a1c
David Brown <browdavi@arch04.cis.gvsu.edu>
2017-04-14 00:30:08 -0400

    i added some queries

--- queries.sql
+++ queries.sql
@@ -1,16 +1,24 @@
 -- Join involving four relations
 --
 -- Output should be
+SELECT 
+FROM player p, team t, teamStats s, university u
+WHERE 
 
 
 -- A self join
 -- 
 -- Output should be 
-
+SELECT p1.height, p1.lname, p1.tID
+FROM player p1, player p2
+WHERE p1.tID = p2.tID
+AND p1.height > p2.height
+ORDER BY p1.tID, p1.weight, p1.lname
 
 -- UNION, INTERSECT, and/or MINUS
 -- 
 -- Output should be
+
 
 
 -- SUM
@@ -23,9 +31,9 @@ FROM team t;
 -- AVG
 -- Find the average age of all the players on one team (Use Neumann University)
 -- Output should be 21.3
-SELECT AVG(p.age)
-FROM player p, team t
-WHERE t.teamId = 57984 AND p.teamId = t.teamId;
+-- SELECT AVG(p.age)
+-- FROM player p, team t
+-- WHERE t.teamId = 57984 AND p.teamId = t.teamId;
 
 
 -- MAX
@@ -40,24 +48,36 @@ FROM player p;
 SELECT MIN(p.weight)
 FROM player p;
 
+-- Find the average league weight, then find number of players each team has above that weight
 -- GROUP BY, HAVING, and ORDER BY
--- For every team that has more than 20 points, and more than 10 players, give the player name, age, team name, and number of points the team has
--- output should be all players from Farmingdale, West Chester, Massachusetts, and Rhode Island
-SELECT p.name, p.age, t.teamName, s.points
-FROM player p, team t, team_stats s
-WHERE p.teamId = t.teamId
-GROUP BY p.name, p.age
-HAVING count(*) > 12
-AND SUM(s.points > 20)
-ORDER BY t.teamName;
+SELECT t.univName, p.lname, p.weight
+FROM player p, team t
+WHERE t.teamID = p.tID AND p.weight > ANY
+                                     (SELECT AVG(p.weight)
+                                      FROM player p2)
+GROUP BY t.univName
+HAVING count(*) > 5
+ORDER BY p.weight
+
+--SELECT p.name, p.age, t.teamName, s.points
+--FROM player p, team t, team_stats s
+--WHERE p.teamId = t.teamId
+--GROUP BY p.name, p.age
+--HAVING count(*) > 10
+--AND SUM(s.points > 20)
+--ORDER BY t.teamName;
 
 
 -- Correlated subquery
 -- Find every player whose age is above the team average (Use Farmingdale State)
 -- Output should be
-SELECT 
-FROM
-WHERE
+SELECT DISTINCT p.pid, p.lname
+FROM player p
+WHERE NOT EXISTS (SELECT gp.goalsScored
+		  FROM games_played
+		  WHERE gp.pID = p.pid
+		  AND gp.goalsScored < 20)
+ORDER BY e.lname;
 
 
 -- Non-Correlated subquery
